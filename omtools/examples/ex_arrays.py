e8485564addbdf80379b43a84fcf6d718b42e6c6
AlexKIvanov <alivanov@ucsd.edu>
2021-01-18 10:50:48 -0800

    Added sum to test_array. Removed old test_sum.

--- omtools/examples/ex_arrays.py
+++ omtools/examples/ex_arrays.py
@@ -67,9 +67,8 @@ class ExampleMax(Group):
         o = 4
         p = 5
         q = 6
-        """
-        Scalar and Axis-wise maximum of a tensor across the (1,3) axis
-        """
+
+        ## Scalar and Axis-wise maximum of a tensor across the (1,3) axis
 
         # Shape of a tensor
         tensor_shape = (m, n, o, p, q)
@@ -87,9 +86,9 @@ class ExampleMax(Group):
 
         # Computing the minimum across the entire tensor, returns single value
         self.register_output('ScalarMax', ot.max(ten1))
-        """
-        Element-wise minimum between three tensors
-        """
+
+        ## Element-wise minimum between three tensors
+
         m = 2
         n = 3
         # Shape of the three tensors is (2,3)
@@ -164,3 +163,83 @@ class ExampleReorderAxes(Group):
         # given by 'ijkl->ljki'
         self.register_output('axes_reordered_tensor',
                              ot.reorder_axes(tens, 'ijkl->ljki'))
+
+
+class ExampleSum(Group):
+    """
+    :param var: single_vector_sum
+    :param var: single_matrix_sum
+    :param var: single_tensor_sum
+    :param var: multiple_vector_sum
+    :param var: multiple_matrix_sum
+    :param var: multiple_tensor_sum
+    :param var: single_matrix_sum_along_0
+    :param var: single_matrix_sum_along_1
+    :param var: multiple_matrix_sum_along_0
+    :param var: multiple_matrix_sum_along_1
+    
+    """
+    def setup(self):
+        n = 3
+        m = 6
+        p = 7
+        q = 10
+
+        # Declare a vector of length 3 as input
+        v1 = self.declare_input('v1', val=np.arange(n))
+
+        # Declare another vector of length 3 as input
+        v2 = self.declare_input('v2', val=np.arange(n, 2 * n))
+
+        # Declare a matrix of shape 3x6 as input
+        M1 = self.declare_input('M1', val=np.arange(n * m).reshape((n, m)))
+
+        # Declare another matrix of shape 3x6 as input
+        M2 = self.declare_input('M2',
+                                val=np.arange(n * m, 2 * n * m).reshape(
+                                    (n, m)))
+
+        # Declare a tensor of shape 3x6x7x10 as input
+        T1 = self.declare_input('T1',
+                                val=np.arange(n * m * p * q).reshape(
+                                    (n, m, p, q)))
+
+        # Declare another tensor of shape 3x6x7x10 as input
+        T2 = self.declare_input('T2',
+                                val=np.arange(n * m * p * q,
+                                              2 * n * m * p * q).reshape(
+                                                  (n, m, p, q)))
+
+        # Output the sum of all the elements of the vector v1
+        self.register_output('single_vector_sum', ot.sum(v1))
+
+        # Output the sum of all the elements of the matrix M1
+        self.register_output('single_matrix_sum', ot.sum(M1))
+
+        # Output the sum of all the elements of the tensor T1
+        self.register_output('single_tensor_sum', ot.sum(T1))
+
+        # Output the elementwise sum of vectors v1 and v2
+        self.register_output('multiple_vector_sum', ot.sum(v1, v2))
+
+        # Output the elementwise sum of matrices M1 and M2
+        self.register_output('multiple_matrix_sum', ot.sum(M1, M2))
+
+        # Output the elementwise sum of tensors T1 and T2
+        self.register_output('multiple_tensor_sum', ot.sum(T1, T2))
+
+        # Output the axiswise sum of matrix M1 along the columns
+        self.register_output('single_matrix_sum_along_0', ot.sum(M1,
+                                                                 axes=(0, )))
+
+        # Output the axiswise sum of matrix M1 along the rows
+        self.register_output('single_matrix_sum_along_1', ot.sum(M1,
+                                                                 axes=(1, )))
+
+        # Output the elementwise sum of the axiswise sum of matrices M1 ad M2 along the columns
+        self.register_output('multiple_matrix_sum_along_0',
+                             ot.sum(M1, M2, axes=(0, )))
+
+        # Output the elementwise sum of the axiswise sum of matrices M1 ad M2 along the rows
+        self.register_output('multiple_matrix_sum_along_1',
+                             ot.sum(M1, M2, axes=(1, )))
